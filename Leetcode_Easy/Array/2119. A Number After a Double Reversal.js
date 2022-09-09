a16fbd9343fa464568d8bb19743a43ef7c59e9a2
kimmy <hiyoung.kimmy@gmail.com>
2022-09-09 14:57:37 -0700

    Coding Challenge : 09-09

---
+++ Leetcode_Easy/Array/2119. A Number After a Double Reversal.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+ var isSameAfterReversals = function(num) {
+    let reversed1 = 0;
+    let reversed2 = 0;
+    let remain = Math.abs(num);
+    
+    
+      //예외처리
+      if(num === 0) return null;
+      
+      
+      
+     while(remain !== 0){
+      reversed1 *= 10;
+      reversed1 += remain % 10;
+      remain = Math.floor(remain/10);
+    }
+    
+    remain = Math.abs(reversed1);
+  
+    while(remain !== 0) {
+      reversed2 *= 10;
+      reversed2 += remain %10;
+      remain = Math.floor(remain/10);
+    }
+    
+   if(reversed2 === num) return true;
+    else return false;
+    
+  
+  };
