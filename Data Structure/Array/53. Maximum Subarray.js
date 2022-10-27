4d66f6aed2807a0cfa0c01e14c154030dba936bc
kimmy <hiyoung.kimmy@gmail.com>
2022-10-27 15:39:22 -0700

    Data Structure : 10-27

--- Data Structure/Array/53. Maximum Subarray.js
+++ Data Structure/Array/53. Maximum Subarray.js
@@ -3,16 +3,36 @@
  * @return {number}
  */
  var maxSubArray = function(nums) {
-    let helper = [nums[0]];
-     let left = 0;
-     
-     if(nums.length === 1) return nums[0];
-     
-     for(let i=1; i<nums.length; i++) {
-         helper.push(Math.max(nums[i], helper[i-1]+nums[i]));
-     }
-     
-     return Math.max(...helper);  
- };
- 
- 
+    let localMax = nums[0];
+    let globalMax = nums[0];
+    
+    for(let i = 1; i < nums.length; i++) {
+    	localMax = Math.max(nums[i], localMax + nums[i]);
+        if(localMax > globalMax) {
+        	globalMax = localMax;
+        }
+    }
+    
+    return globalMax;
+};
+
+
+maxSubArray([-2,1,-3,4]); 
+
+
+//Dynamic Programming Algorithm : Largest Sum Contiguous Subarray (Kadane’s Algorithm)
+//Time Coplexity : O(N) Speed : 80ms
+
+
+// helper = -2
+// -2, Math.max(1, -2+1)
+
+// helper = -2, 1
+// -2, 1, Math.max(-3,1+-3)
+
+// helper = -2, 1, -2
+// -2 , 1, -2, Math.max(4, -2+4)
+
+// helper = -2, 1, -2, 4
+
+// answer = 4;
